df818c8dfe519ae1ddf1d6bcc37e6b2f2be79390
basile du Plessis <basile.duplessis@gmail.com>
2023-10-09 11:35:21 +0200

    Rust Stack Kata done

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,208 @@
 fn main() {
-    println!("Hello, world!");
+    let mut stack = BoundedStack::new(2);
+    stack.push(1);
+    stack.push(1);
+    stack.push(1);
 }
 
-struct Stack{
+
+struct BoundedStack {
     size: usize,
-    capacity: usize
+    capacity: usize,
+    elements: Vec<usize>,
 }
 
-impl Stack {
-    fn new(capacity: usize) -> Stack {
-        Stack {
-            size: 0,
-            capacity
-        }
-    }
+struct ZeroCapacityStack {
+}
+
+trait Stack {
+    fn is_empty(&self) -> bool;
+    fn push(&mut self, element: usize);
+    fn pop(&mut self) -> usize;
+    fn get_size(&self) -> usize;
+    fn top(&self) -> usize;
+    fn find(&self, element: usize) -> Option<usize>;
+}
+
+impl Stack for BoundedStack {
     fn is_empty(&self) -> bool {
         self.size == 0
+    }
+    fn push(&mut self, element: usize) {
+        if self.size == self.capacity {
+            panic!("Capacity overflow");
+        }         
+        self.elements.push(element);
+        self.size += 1;
+    }
+    fn pop(&mut self) -> usize {
+        if self.is_empty() {
+            panic!("Capacity underflow");
+        }
+        self.size -= 1;
+        self.elements[self.size]
+    }
+    fn top(&self) -> usize {
+        if self.is_empty() {
+            panic!("Empty"); 
+        }
+        self.elements[self.size - 1]
     }
     fn get_size(&self) -> usize {
         self.size
     }
-    fn push(&mut self, value:usize) {
-        if self.size == self.capacity {
-            panic!("Capacity overflow");
-        }            
-        self.size += 1;
-    }
-    fn pop(&mut self) {
-        if self.size == 0 {
-            panic!("Capacity overflow");
+    fn find(&self, element: usize) -> Option<usize> {
+        for i in (0..=(self.size-1)).rev() {
+            if self.elements[i] == element {
+                return Some(self.size-1-i);
+            }
         }
-        self.size -= 1;
+        None
     }
 }
 
+impl Stack for ZeroCapacityStack {
+    fn get_size(&self) -> usize {
+        0
+    }
+    fn is_empty(&self) -> bool {
+        true
+    }
+    fn pop(&mut self) -> usize {
+        panic!("Capacity underflow");
+    }
+    fn top(&self) -> usize {
+        panic!("Empty"); 
+    }
+    fn push(&mut self, element: usize) {
+        panic!("Capacity overflow");
+    }
+    fn find(&self, element: usize) -> Option<usize> {
+        None
+    }
+}
+
+impl BoundedStack {
+    fn new(capacity: usize) -> Box<dyn Stack> {
+        if capacity == 0 {
+            return Box::new(
+                ZeroCapacityStack {}
+            );
+        }
+        Box::new(BoundedStack {size: 0, capacity, elements: Vec::with_capacity(capacity) })
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     struct TestContext {
-        stack: Stack
+        stack: Box<dyn Stack>,
     }
 
     impl TestContext {
         fn setup() -> TestContext {
             TestContext {
-                stack : Stack::new(2)
+                stack: BoundedStack::new(2),
             }
         }
     }
     #[test]
     fn newly_create_stack_should_be_sempty() {
-        let test_context = TestContext::setup();
-        assert!(test_context.stack.is_empty());
-        assert_eq!(test_context.stack.get_size(), 0);
+        let stack = TestContext::setup().stack;
+        assert!(stack.is_empty());
+        assert_eq!(stack.get_size(), 0);
     }
 
     #[test]
     fn after_one_push_stack_size_should_be_one() {
-        let mut test_context = TestContext::setup();
-        test_context.stack.push(1);
-        assert_eq!(test_context.stack.get_size(), 1);
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        assert_eq!(stack.get_size(), 1);
     }
 
     #[test]
     fn after_one_push_and_one_pop_stack_size_should_be_empty() {
-        let mut test_context = TestContext::setup();
-        test_context.stack.push(1);
-        test_context.stack.pop();
-        assert!(test_context.stack.is_empty());
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        stack.pop();
+        assert!(stack.is_empty());
     }
 
     #[test]
     #[should_panic]
     fn when_pushed_past_limit_stack_overflows() {
-        let mut test_context = TestContext::setup();
-        test_context.stack.push(1);
-        test_context.stack.push(1);
-        test_context.stack.push(1);
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        stack.push(1);
+        stack.push(1);
     }
 
     #[test]
     #[should_panic]
     fn when_empty_stack_is_popped_stack_overflows() {
-        let mut test_context = TestContext::setup();
-        test_context.stack.pop();
-        test_context.stack.pop();
-        test_context.stack.pop();
+        let mut stack = TestContext::setup().stack;
+        stack.pop();
+        stack.pop();
+        stack.pop();
     }
 
-}
+    #[test]
+    fn when_one_is_pushed_one_is_popped() {
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        assert_eq!(stack.pop(), 1);
+    }
 
+    #[test]
+    fn when_one_two_is_pushed_two_one_is_popped() {
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        stack.push(2);
+        assert_eq!(stack.pop(), 2);
+        assert_eq!(stack.pop(), 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn when_creating_stack_with_zero_capacity_every_push_should_overflow() {
+        let mut stack = BoundedStack::new(0);
+        stack.push(1);
+    }
+
+    #[test]
+    fn when_one_is_pushed_one_is_on_top() {
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        assert_eq!(stack.top(), 1);
+
+    }
+    #[test]
+    #[should_panic]
+    fn when_stack_is_empty_top_throws_empty() {
+        let stack = TestContext::setup().stack;
+        stack.top();
+    }
+    #[test]
+    #[should_panic]
+    fn with_zero_capacity_stack_any_top_throws_empty() {
+        let mut stack = BoundedStack::new(0);
+        stack.top();
+    }
+    #[test]
+    fn given_stack_with_one_two_pushed_find_one() {
+        let mut stack = TestContext::setup().stack;
+        stack.push(1);
+        stack.push(2);
+        assert_eq!(stack.find(1).unwrap(), 1);
+        assert_eq!(stack.find(2).unwrap(), 0);
+    }
+    #[test]
+    fn with_zero_capacity_stack_find_throws_none() {
+        let stack = BoundedStack::new(0);
+        assert_eq!(stack.find(2), None);
+    }
+}
